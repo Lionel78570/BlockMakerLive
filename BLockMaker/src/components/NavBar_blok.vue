db9f7cc6d5a81b3c7d9bf7e4562df4f5103f977f
Arthur Pouteau <arthur.pouteau92@gmail.com>
2024-01-09 15:12:06 +0100

    add Navbar component

---
+++ BLockMaker/src/components/NavBar_blok.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+      <ul>
+        <NavItem v-for="link in links" :key="link.path" :link="link" />
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  import NavItem from './NavItem.vue'
+  
+  export default {
+    name: 'NavBar',
+    components: {
+      NavItem
+    },
+    props: {
+      links: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  nav {
+    /* Ajoutez votre style ici */
+  }
+  </style>
