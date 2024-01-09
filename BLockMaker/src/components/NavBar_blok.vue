564e55f278c4036be4ddf1a894930a2262b23deb
Arthur Pouteau <arthur.pouteau92@gmail.com>
2024-01-09 16:18:57 +0100

    add securtiy.md issue template jest babel & eslint files

--- BLockMaker/src/components/NavBar_blok.vue
+++ BLockMaker/src/components/NavBar_blok.vue
@@ -1,30 +1,33 @@
 <template>
-    <nav>
-      <ul>
-        <NavItem v-for="link in links" :key="link.path" :link="link" />
-      </ul>
-    </nav>
-  </template>
-  
-  <script>
-  import NavItem from './NavItem.vue'
-  
-  export default {
-    name: 'NavBar',
-    components: {
-      NavItem
-    },
-    props: {
-      links: {
-        type: Array,
-        required: true
-      }
+  <nav>
+    <ul>
+      <li v-for="link in links" :key="link.path">
+        <router-link :to="link.path">{{ link.text }}</router-link>
+        <DropDown_Blok v-if="link.submenu" :links="link.submenu" />
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import DropDown_Blok from './DropDown_Blok.vue';
+
+export default {
+  name: 'NavBar',
+  components: {
+    DropDown_Blok
+  },
+  props: {
+    links: {
+      type: Array,
+      required: true
     }
   }
-  </script>
-  
-  <style scoped>
-  nav {
-    /* Ajoutez votre style ici */
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+nav {
+  /* Ajoutez votre style ici */
+}
+</style>
